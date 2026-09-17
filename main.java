c94aef47427e4ccfdfbc588c9c0cb7381a99561c
shrejahn <shreya.25bai11620@vitbhopal.ac.in>
2026-09-17 23:44:12 +0530

    Add main class to initialize ReservationSystem

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        ReservationSystem system =
+                new ReservationSystem();
+
+        system.start();
+    }
+}
